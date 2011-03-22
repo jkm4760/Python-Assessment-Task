42e809bf29ea71a823d08871340a6d1997cfdfaa
AClockWorkLemon <info.clockworkgames@gmail.com>
2011-03-22 17:24:03 +1100

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+#################################
+#     Python Assesment Task 1:  #
+#            Hangman            #
+#################################
+
+#Entry Function
+def Main():
+    #Dummy function
+
+#Application Entry Point
+Main()
