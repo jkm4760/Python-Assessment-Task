3fca5aa8b6b85dad79996ffb086542240e268e7b
AClockWorkLemon <info.clockworkgames@gmail.com>
2011-03-22 17:51:29 +1100

    Reading input into a list

--- Main.py
+++ Main.py
@@ -3,9 +3,20 @@
 #            Hangman            #
 #################################
 
+#Main Game function
+def Game(word):
+    #Read the word into a list for easy editing
+    word_ls = []
+    for char in word:
+        word_ls.append(char.capitalize()) #capitalize everything so as to avoid conflicts
+
+    #Test word list
+    print word_ls
+
 #Entry Function
 def Main():
-    #Dummy function
+    inp = raw_input('(TEMP) Enter Word: ')
+    Game(inp)
 
 #Application Entry Point
 Main()
